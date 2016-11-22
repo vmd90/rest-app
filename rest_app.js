1bafabb8ef0a0a43630c42f2ca6b5adf8153b16e
Victor <victor.munidario@gmail.com>
2016-11-22 14:21:03 -0200

    implementing ajax get

--- rest_app.js
+++ rest_app.js
@@ -1,5 +1,16 @@
 
 function search()
 {
-	
+	var id = $('#user-id-input').val();
+	console.log(id);
+	$.ajax({
+		url: 'http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com/hirers/'+id+'/opportunities',
+		type: 'GET',
+		success: function (data) {
+			$('#result').html(data);
+		},
+		error: function (data) {
+			alert(data);
+		}
+	});
 }
